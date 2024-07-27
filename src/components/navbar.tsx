ec7103df065c7b5e0ad4608d46480a1f658993aa
Andrew Virts <asvirts@outlook.com>
2024-07-27 13:20:45 -0500

    nextjs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='w-100 p-3'>
+    <div>
+      <div className='flex items-center justify-between p-4'>
       <h1>orbit</h1>
+      <ul className='flex items-center justify-between'>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </div>
     </div>
   )
 }
